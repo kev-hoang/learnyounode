ee358f0fba461bd713cae565058d1213ffff616f
Kevin Hoang <find.kevinhoang@gmail.com>
2015-05-31 19:39:19 -0400

    move the readFile down.

--- myFirstAsyncI_O.js
+++ myFirstAsyncI_O.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 
-fs.readFile(process.argv[2], "utf8", callback)
-
 function callback(err,data) {
   var arr = data.split("\n");
   console.log(arr.length - 1);
 }
+
+fs.readFile(process.argv[2], "utf8", callback)
